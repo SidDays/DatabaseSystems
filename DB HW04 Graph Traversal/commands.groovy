66656e9d5c63c2ef2f6d29fcd207de985a0fbef0
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-11-24 01:59:26 -0800

    HW4 submit version, HW5 description added

--- DB HW04 Graph Traversal/commands.groovy
+++ DB HW04 Graph Traversal/commands.groovy
@@ -1,20 +1,33 @@
+// Siddhesh Rajiv Karekar
+// karekar@usc.edu
+
 // Q1. Create the graph
-graph = TinkerGraph.open()
-g = graph.traversal()
-cs101 = g.addV("CS101").next()
-cs201 = g.addV("CS201").next()
-cs220 = g.addV("CS220").next()
-cs334 = g.addV("CS334").next()
-cs400 = g.addV("CS400").next()
-cs420 = g.addV("CS420").next()
-cs526 = g.addV("CS526").next()
-cs681 = g.addV("CS681").next()
-g.addE("requires pre-req").from(cs201).to(cs101)
-g.addE("requires pre-req").from(cs220).to(cs201)
-g.addE("requires pre-req").from(cs420).to(cs220)
-g.addE("requires pre-req").from(cs334).to(cs201)
-g.addE("requires pre-req").from(cs400).to(cs334)
-g.addE("requires pre-req").from(cs681).to(cs334)
-g.addE("requires pre-req").from(cs526).to(cs400)
-g.addE("is a co-req of").from(cs420).to(cs220)
-g.addE("is a co-req of").from(cs526).to(cs400)
+g = TinkerGraph.open().traversal()
+g.addV('course').property(id, 'CS101').as('CS101').
+addV('course').property(id, 'CS201').as('CS201').
+addV('course').property(id, 'CS220').as('CS220').
+addV('course').property(id, 'CS334').as('CS334').
+addV('course').property(id, 'CS400').as('CS400').
+addV('course').property(id, 'CS420').as('CS420').
+addV('course').property(id, 'CS526').as('CS526').
+addV('course').property(id, 'CS681').as('CS681').
+addE('requires pre-req').from('CS201').to('CS101').
+addE('requires pre-req').from('CS220').to('CS201').
+addE('requires pre-req').from('CS420').to('CS220').
+addE('requires pre-req').from('CS334').to('CS201').
+addE('requires pre-req').from('CS400').to('CS334').
+addE('requires pre-req').from('CS681').to('CS334').
+addE('requires pre-req').from('CS526').to('CS400').
+addE('is a co-req of').from('CS420').to('CS220').
+addE('is a co-req of').from('CS526').to('CS400').iterate()
+
+// Q2. Write a query that will output JUST the doubly-connected nodes.
+g.V().as('a').outE().inV().as('b').select('a','b').groupCount().unfold().filter(select(values).is(eq(2))).select(keys)
+
+// Q3. Write a query that will output all the ancestors (for us, these would be prerequisites) of a given vertex.
+g.V().has(id,'CS526').repeat(out().dedup()).emit()
+
+// Q4. Write a query that will output the max depth starting from a given node (provides a count (including itself) of all the connected nodes till the deepest leaf).
+g.V().has(id,'CS101').repeat(__.in()).emit().path().count(local).max()
+
+// Alternative Bonus: Queries 2-4 were done in a single command and query 1 in two commands instead of the Eulerian Circuit problem.
